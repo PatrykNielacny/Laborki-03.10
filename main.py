643907a46bc0750bb7547723c4c15da6a34dbbbe
PatrykNielacny <100844107+PatrykNielacny@users.noreply.github.com>
2022-03-10 12:15:54 +0000

    Added codes

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+print("Sa jeszcze zadania dodatkowe aby je wyswietlic wpisz dodatkowe i nr zadania np dodatkowe1\n")
 print("Aby sprawdzic poprawnosc zadania wpisz zd z numerem ktory chcesz sprawdzic np: zd1, zd2 ...")
 zadanie = input()
 # Zadanie 1
@@ -64,28 +65,58 @@ if(zadanie == "zd7"):
   x = input()
   if(x=="+"):
     print("Wynik dodawania to: ")
-    print(a+b)
+    print(a + b)
   if(x=="-"):
     print("Wynik odejmowania to: ")
-    print(a-b)
+    print(a - b)
   if(x=="*"):
     print("Wynik mnozenia to: ")
-    print(a*b)
+    print(a * b)
   if(x=="/"):
     print("Wynik dzielenia to: ")
-    print(a/b)
+    print(a / b)
   if(x=="**"):
     print("Wynik potegowania to: ")
-    print(a**b)
+    print(a ** b)
   if(x=="//"):
     print("Wynik dzielenia calkowitego: ")
-    print(a//b)
+    print(a // b)
   if(x=="%"):
     print("Reszta z dzielenia a//b to: ")
-    print(a%b)
+    print(a % b)
 # Zadanie 8
 if(zadanie == "zd8"):
   print("Podaj liczbe x: ")
   x = int(input())
   print("Wynik x > 1 i x < 13 to: ", x > 1 and x < 13)
   print("Wynik dla x != 5 lub x < 0 to: ", x != 5 or x < 0)
+if(zadanie == "dodatkowe1"):
+  print("Podaj imie: ")
+  imie = input()
+  print("Podaj nazwisko: ")
+  nazwisko = input()
+  print("Podaj wiek: ")
+  wiek = int(input())
+  print("\nTeraz zostanie zadanych ci 5 pytania sa to pytania w stylu tak / nie\n")
+  print("Czy zdrowo sie odżywiasz ? ")
+  p1 = input()
+  print("\nCzy lubisz sport ? ")
+  p2 = input()
+  print("\nCzy pijesz dziennie 3 szklanki wody ? ")
+  p3 = input()
+  print("\nCzy spożywasz alkohol częściej niz 2-3 razy w miesiącu ? ")
+  p4 = input()
+  print("\nOstatnie pytanie.")
+  print("\nCzy często zastępujesz samochód rowerem ? ")
+  p5 = input()
+  if(p1 == "tak" and p2 == "tak" and p3 == "tak" and p4 == "nie" and p5 == "tak"):
+    print("Pan " + imie + " " + nazwisko + " mając " + str(wiek) + " lat jest jedna z najzdrowiej żyjących osób jakie znam.")
+  if(p1 == "nie" and p2 == "nie" and p3 == "nie" and p4 == "tak" and p5 == "nie"):
+    print("Pan " + imie + " " + nazwisko + " mając " + str(wiek) + " lat jest jedna z najgorzej żyjących osób jakie znam.")
+  else:
+    print("Mogło być lepiej ale nie jest najgorzej")
+if(zadanie == "dodatkowe2"):
+  print("Podaj imie: ")
+  imie = input()
+  print("Podaj nazwisko: ")
+  nazwisko = input()
